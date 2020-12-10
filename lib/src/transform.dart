d8bf01feb1298ab896896d4e16d5d21bfa006b4d
wangziyang <457155134@qq.com>
2020-12-10 09:17:41 +0800

    moved transform.dart from src

--- lib/src/transform.dart
+++
@@ -1,4 +0,0 @@
-export 'annotation/reflect.dart';
-export 'annotation/function_log.dart';
-export 'annotation/click_shake.dart';
-export 'tool/shake_handle_help.dart' ;
---
+++ lib/transform.dart
@@ -0,0 +1,4 @@
+export 'package:ft/src/annotation/reflect.dart';
+export 'package:ft/src/annotation/function_log.dart';
+export 'package:ft/src/annotation/click_shake.dart';
+export 'package:ft/src/tool/shake_handle_help.dart' ;
