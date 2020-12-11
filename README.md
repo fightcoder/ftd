483f5d172dada70a8ddc8ac426a66fbc9ec75056
wangziyang <457155134@qq.com>
2020-12-11 17:45:42 +0800

    commit patch file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### Flutter Dill Transform
 
-提供多个实用的注解以供开发。
+提供多个实用的注解以供开发。Thanks Aspectd : https://github.com/alibaba-flutter/aspectd
 
 #### 1.ClickShake注解，作用于方法：
 
@@ -12,7 +12,20 @@
 
 打印方法耗时，参数等等。
 
+```dart
+@FunctionLog()
+void _incrementCounter(String p1,int p2){
+}
+//输出
+//--> _incrementCounter(String p1 = p1int p2 = 2)
+//<-- _incrementCounter cost time(耗时) = 0[ms]
+```
+
 使用方法：
 
 1. 直接将该项目作为本地库依赖
 2. 对`flutter tools`进行patch
+
+
+
+暂时与Aspectd不能两者完全兼容，需要自己下载源码修改兼容。
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:ft/ft.dart';
 
@@ -45,16 +44,8 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   int i = 0;
 
-  @ClickShake()
+  @FunctionLog()
   void _incrementCounter(String p1,int p2){
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
   }
   bool flag = false;
 
